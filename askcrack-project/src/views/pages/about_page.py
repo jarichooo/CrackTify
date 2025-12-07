df345fa148bd682684a5918a828fb875d9077fe1
johnlouie2004 <jlbearisbagaporo@gmail.com>
2025-12-07 10:14:25 +0800

    Update: About Us Page

--- askcrack-project/src/views/pages/about_page.py
+++ askcrack-project/src/views/pages/about_page.py
@@ -15,15 +15,59 @@ class AboutPage:
             ft.Text(
                 "This application is designed to help users find answers to their questions, mainly cracks on walls related questions",
                 theme_style="bodyMedium",
+                text_align=ft.TextAlign.CENTER,
             ),
             ft.Divider(height=20, opacity=0),
             ft.Text("App Version: 1.0.0", theme_style="bodySmall"),
             ft.Divider(height=20, opacity=0),
         ]
 
-        # Developers Section with GitHub links
+        # Academic Fulfillment Section
+        academic_fulfillment = ft.Column(
+            controls=[
+                ft.Text(
+                    "Project Fulfillment", 
+                    theme_style="titleMedium"
+                ),
+                ft.Text(
+                    "This project is developed in partial fulfillment of the requirements for the following courses:",
+                    theme_style="bodyMedium",
+                    text_align=ft.TextAlign.CENTER
+                ),
+                ft.Divider(height=5, opacity=0),
+                ft.Column(
+                    controls=[
+                        ft.Text("🧑‍💻 Application Development and Emerging Technologies", theme_style="bodySmall"),
+                        ft.Text("🔐 Information Assurance and Security", theme_style="bodySmall"),
+                        ft.Text("🧑‍💻 Software Engineering 1", theme_style="bodySmall"),
+                    ],
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER, 
+                    spacing=2
+                )
+            ],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            spacing=5
+        )
+
+        # Repository Link Section
+        repository_section = ft.Column(
+            controls=[
+                ft.Divider(height=20, opacity=0),
+                ft.Text("CrackTify", theme_style="titleMedium"),
+                ft.TextButton(
+                    text="View on GitHub 🔗",
+                    icon=ft.Icons.LINK,
+                    on_click=lambda e: self.page.launch_url("https://github.com/jarichooo/CrackTify")
+                )
+            ],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            spacing=5
+        )
+
+        # Developers Section
         developers = ft.Column(
             controls=[
+                ft.Divider(height=20, opacity=0),
                 ft.Text("Developers", theme_style="titleMedium"),
                 ft.Row(
                     controls=[
@@ -33,33 +77,36 @@ class AboutPage:
                         ),
                         ft.TextButton(
                             text="Joshua Jericho Barja",
-                            on_click=lambda e: self.page.launch_url("https://https://github.com/jarichooo")
+                            on_click=lambda e: self.page.launch_url("https://github.com/jarichooo")
                         ),
                         ft.TextButton(
                             text="Ven Lavapie",
-                            on_click=lambda e: self.page.launch_url("https://https://github.com/ven-62")
+                            on_click=lambda e: self.page.launch_url("https://github.com/ven-62")
                         ),
                     ],
                     alignment=ft.MainAxisAlignment.CENTER,
                     spacing=20
                 ),
             ],
-            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             spacing=5
         )
 
         # Contact Section
         contact_section = ft.Column(
             controls=[
+                ft.Divider(height=20, opacity=0),
                 ft.Text("Contact / Support", theme_style="titleMedium"),
                 ft.Text("Email: cracktify.noreply@gmail.com", theme_style="bodySmall"),
             ],
-            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             spacing=5
         )
 
+        # Combine all sections
         container = ft.Column(
-            controls=self.content + [developers, contact_section],
+            # In Order: App Info, Academic, Repository, Developers, Contact
+            controls=self.content + [academic_fulfillment, repository_section, developers, contact_section],
             alignment=ft.MainAxisAlignment.CENTER,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             spacing=15
