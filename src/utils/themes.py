aa1051db4d5d39156dcdfd6aabed33fa68b3a711
ven-62 <152240057+ven-62@users.noreply.github.com>
2026-02-10 21:09:11 +0800

    adjust theme mode button logic

--- src/utils/themes.py
+++ src/utils/themes.py
@@ -1,13 +1,15 @@
 import flet as ft
 
-def toggle_theme(page: ft.Page, button: ft.IconButton = None):
+def toggle_theme(page: ft.Page, theme_button: ft.IconButton = None):
     """Toggles between light and dark themes."""
-    if button.icon == ft.Icons.DARK_MODE:
-        page.theme_mode = ft.ThemeMode.LIGHT
-        button.icon = ft.Icons.LIGHT_MODE
+    current_theme = page.theme_mode
+
+    if current_theme == ft.ThemeMode.LIGHT:
+        page.theme_mode = ft.ThemeMode.DARK
+        theme_button.icon = ft.Icons.DARK_MODE
 
     else:
-        page.theme_mode = ft.ThemeMode.DARK
-        button.icon = ft.Icons.DARK_MODE
-
+        page.theme_mode = ft.ThemeMode.LIGHT
+        theme_button.icon = ft.Icons.LIGHT_MODE
+        
     page.update()
